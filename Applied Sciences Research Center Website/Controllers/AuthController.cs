baaa42d35ac26002cc943b4366d836c148a78335
Muzzammil Imran <m.muzzammil.imran@gmail.com>
2025-01-04 18:09:22 +0500

    Auth part complete
    - Mongodb setup
    - Jwt Tokenization added
    - Add + update + delete user added
    - Models + Controllers + services added

--- Applied Sciences Research Center Website/Controllers/AuthController.cs
+++ Applied Sciences Research Center Website/Controllers/AuthController.cs
@@ -1,16 +1,55 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Applied_Sciences_Research_Center_Website.Models;
+using Applied_Sciences_Research_Center_Website.Services;
+using Applied_Sciences_Research_Center_Website.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 namespace Applied_Sciences_Research_Center_Website.Controllers
 {
     [Route("api/[controller]")]
     public class AuthController : Controller
     {
-        [AllowAnonymous]
-        [HttpGet("check")]
-        public ActionResult check()
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection<UserModel> _usersCollection;
+        private readonly AuthService authService;
+
+        public AuthController(IOptions<DatabaseConfigModel> database, IOptions<AuthConfigModel> authConfig)
         {
-            return Ok("All working alhamdililah");
+            var mongoClient = new MongoClient(database.Value.ConnectionString);
+            _database = mongoClient.GetDatabase(database.Value.DataBase);
+            _usersCollection = _database.GetCollection<UserModel>("Users");
+            authService = new AuthService(authConfig);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("Login")]
+        public ActionResult Login(LoginViewModel loginVM)
+        {
+            if (loginVM.Email == null || loginVM.Password == null)
+                return StatusCode(StatusCodes.Status400BadRequest);
+
+            loginVM.Email = loginVM.Email.ToLower();
+
+            var user = _usersCollection.Find(x => x.Email == loginVM.Email).FirstOrDefault();
+
+            if (user == null)
+                return StatusCode(StatusCodes.Status401Unauthorized, "Email is not registered");
+
+            if (user.Password != authService.EncryptPassword(loginVM.Password))
+                return StatusCode(StatusCodes.Status401Unauthorized, "Password is incorrect");
+
+            UserViewModel loggedInUser = new()
+            {
+                Email = user.Email,
+                Password = user.Password,
+                Name = user.Name,
+                Token = authService.GenerateToken(loginVM.Email, user.Role),
+                Role = user.Role
+            };
+
+            return Ok(loggedInUser);
         }
     }
 }
---
+++ Applied Sciences Research Center Website/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+
+namespace Applied_Sciences_Research_Center_Website.ViewModels
+{
+    public class UserViewModel
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string Role { get; set; }
+        public string? Token { get; set; }
+        public ObjectId id { get; set; }
+        public int RequestCount { get; set; }
+    }
+}
