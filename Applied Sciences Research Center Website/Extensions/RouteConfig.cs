9732d8c6ae7577a3d3b442a72b1f4e345868429d
Muzzammil Imran <m.muzzammil.imran@gmail.com>
2025-01-04 15:51:23 +0500

    wwwroot files added

---
+++ Applied Sciences Research Center Website/Extensions/RouteConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace Applied_Sciences_Research_Center_Website.Extensions
+{
+    public static class RouteConfig
+    {
+        public static void ConfigureRoutes(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/", async context =>
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response.SendFileAsync("wwwroot/html/index.html");
+            });
+        }
+    }
+}
--- Applied Sciences Research Center Website/Program.cs
+++ Applied Sciences Research Center Website/Program.cs
@@ -1,6 +1,15 @@
+using Applied_Sciences_Research_Center_Website.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.ConfigureRoutes();
 
 app.Run();
