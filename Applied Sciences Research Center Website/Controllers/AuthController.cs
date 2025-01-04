62f55977f74f23c7e34cf5bdc9ece19c560dd97a
Muzzammil Imran <m.muzzammil.imran@gmail.com>
2025-01-04 16:59:45 +0500

    Backend: Swagger added

---
+++ Applied Sciences Research Center Website/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Applied_Sciences_Research_Center_Website.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet("check")]
+        public ActionResult check()
+        {
+            return Ok("All working alhamdililah");
+        }
+    }
+}
--- Applied Sciences Research Center Website/Program.cs
+++ Applied Sciences Research Center Website/Program.cs
@@ -3,11 +3,32 @@ using Applied_Sciences_Research_Center_Website.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy =>
+    {
+        policy.RequireRole("Admin");
+    });
+});
 
 var app = builder.Build();
 
 app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.ConfigureRoutes();
