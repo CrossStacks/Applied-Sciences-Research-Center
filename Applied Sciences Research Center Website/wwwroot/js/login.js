76cd73a418a20509162ff528e36e81e44aadeb88
Muzzammil Imran <m.muzzammil.imran@gmail.com>
2025-01-04 18:27:52 +0500

    Issue fixed

--- Applied Sciences Research Center Website/wwwroot/js/login.js
+++ Applied Sciences Research Center Website/wwwroot/js/login.js
@@ -5,26 +5,18 @@ $(document).ready(function () {
         const email = $('#email').val().trim();
         const password = $('#password').val().trim();
 
-        // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
         // const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-        if (!email) {
-            alert('Email is required.');
-        } else if (!emailRegex.test(email)) {
-            alert('Please enter a valid email address.');
-        } else if (!password) {
-            alert('Password is required.');
-        } else if (!passwordRegex.test(password)) {
-            alert(
-                'Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one special character.'
-            );
-        } else {
-            alert('Sign-In successful! Email: ' + email);
-        }
+        //  if (!password) {
+        //     alert('Password is required.');
+        // } else if (!passwordRegex.test(password)) {
+        //     alert(
+        //         'Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one special character.'
+        //     );
+        // } else {
+        //     alert('Sign-In successful! Email: ' + email);
+        // }
 
-        $('#signInButton').click(function () {
-            console.log('Sign-In successful! Email: ' + email);
-        });
+        console.log('Sign-In successful! Email: ' + email);
     });
 });
