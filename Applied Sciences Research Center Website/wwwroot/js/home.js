dda937fef0cc238d1e46d88e15aa52052ecbbe80
Awaisjeeh <127028984+Awaisjeeh@users.noreply.github.com>
2025-01-05 17:17:13 +0500

    frontend:home screen final

--- Applied Sciences Research Center Website/wwwroot/js/home.js
+++ Applied Sciences Research Center Website/wwwroot/js/home.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
   $("#signup-button").click(function () {
-    window.location.href = "login.html";
+    window.location.href = "signup.html";
+  });
+
+  $(window).on("scroll", function () {
+    var scrollPosition = $(window).scrollTop();
+    $(".navbar-second .nav-link").each(function () {
+      var section = $($(this).attr("href"));
+      if (section.length) {
+        var sectionOffset = section.offset().top - 60;
+        var sectionHeight = section.outerHeight();
+        if (
+          scrollPosition >= sectionOffset &&
+          scrollPosition < sectionOffset + sectionHeight
+        ) {
+          $(".navbar-second .nav-link").removeClass("active");
+          $(this).addClass("active");
+        }
+      }
+    });
+  });
+
+  // Highlight the active link on page load
+  $(window).on("load", function () {
+    $(".navbar-second .nav-link").each(function () {
+      if (window.location.hash === $(this).attr("href")) {
+        $(".navbar-second .nav-link").removeClass("active");
+        $(this).addClass("active");
+      }
+    });
   });
 });
