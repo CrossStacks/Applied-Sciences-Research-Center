00536be56d8ecfdce95f36a310064b3ba9cdca03
Awaisjeeh <127028984+Awaisjeeh@users.noreply.github.com>
2025-01-07 00:00:40 +0500

    front end : nav bar final

--- Applied Sciences Research Center Website/wwwroot/js/home.js
+++ Applied Sciences Research Center Website/wwwroot/js/home.js
@@ -3,31 +3,46 @@ $(document).ready(function () {
     window.location.href = "login";
   });
 
-  $(window).on("scroll", function () {
-    var scrollPosition = $(window).scrollTop();
-    $(".navbar-second .nav-link").each(function () {
-      var section = $($(this).attr("href"));
-      if (section.length) {
-        var sectionOffset = section.offset().top - 60;
-        var sectionHeight = section.outerHeight();
-        if (
-          scrollPosition >= sectionOffset &&
-          scrollPosition < sectionOffset + sectionHeight
-        ) {
-          $(".navbar-second .nav-link").removeClass("active");
-          $(this).addClass("active");
+  $(document).ready(function () {
+    function updateNavbarBrand() {
+      // Update only on smaller screens
+      if ($(window).width() <= 991) {
+        var activeLink = $(".navbar-second .nav-link.active");
+        if (activeLink.length) {
+          var activeText = activeLink.text();
+          $(".navbar-second .navbar-brand").text(activeText);
         }
       }
-    });
-  });
+    }
 
-  // Highlight the active link on page load
-  $(window).on("load", function () {
-    $(".navbar-second .nav-link").each(function () {
-      if (window.location.hash === $(this).attr("href")) {
-        $(".navbar-second .nav-link").removeClass("active");
-        $(this).addClass("active");
-      }
+    // Highlight the active nav-link on scroll and update navbar brand text
+    $(window).on("scroll", function () {
+      var scrollPosition = $(window).scrollTop();
+      $(".navbar-second .nav-link").each(function () {
+        var section = $($(this).attr("href"));
+        if (section.length) {
+          var sectionOffset = section.offset().top - 70; // Adjust offset as needed
+          var sectionHeight = section.outerHeight();
+          if (
+            scrollPosition >= sectionOffset &&
+            scrollPosition < sectionOffset + sectionHeight
+          ) {
+            $(".navbar-second .nav-link").removeClass("active");
+            $(this).addClass("active");
+            updateNavbarBrand();
+          }
+        }
+      });
+    });
+
+    // Highlight the active link and update navbar brand text on page load
+    $(window).on("load", function () {
+      updateNavbarBrand();
+    });
+
+    // Update navbar brand text on window resize
+    $(window).on("resize", function () {
+      updateNavbarBrand();
     });
   });
 });
