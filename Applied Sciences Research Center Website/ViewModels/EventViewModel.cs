66703f0c277e4744ce7beb26c65e1ab2105ad14c
Muzzammil Imran <m.muzzammil.imran@gmail.com>
2025-03-03 02:53:52 +0500

    Backend: Events endpoints added

---
+++ Applied Sciences Research Center Website/ViewModels/EventViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Applied_Sciences_Research_Center_Website.ViewModels
+{
+    public class EventViewModel
+    {
+        public string? ImageUrl { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Date { get; set; }
+        public string? HostName { get; set; }
+    }
+}
