32c2e18bda364d50f94ca7c8e90346bbf1054b4d
Muhammad Junaid Qamar <muhammadjunaidqamar@gmail.com>
2025-03-05 23:09:17 +0500

    added constant file

--- asrc_flutter/lib/main.dart
+++ asrc_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
+import 'utils/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: Constants.appName,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
