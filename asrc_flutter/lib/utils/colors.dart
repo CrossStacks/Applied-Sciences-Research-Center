11d381aed9a9733e1e00ce7b4a163745a87af137
Muzzammil Imran <m.muzzammil.imran@gmail.com>
2025-03-07 23:58:48 +0500

    color update

--- asrc_flutter/lib/utils/colors.dart
+++ asrc_flutter/lib/utils/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color primaryColor = Color.fromARGB(255, 253, 250, 222);
+  static const Color primaryColor = Color.fromARGB(255, 134, 217, 255);
   static const Color secondaryColor = Color.fromARGB(255, 12, 119, 190);
   static const Color tertiaryColor = Color(0xFF607D8B);
   static const Color lightGrey = Color(0xFFE0E0E0);
