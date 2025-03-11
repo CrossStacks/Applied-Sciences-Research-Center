41fcd790ced1b47b1b03bbc76a7ea57cbbc8663f
Muzzammil Imran <m.muzzammil.imran@gmail.com>
2025-03-11 15:30:04 +0500

    Asp.Net Project removal

--- asrc_aps.net/Models/DatabaseConfigModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Applied_Sciences_Research_Center_Website.Models
-{
-    public class DatabaseConfigModel
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string DataBase { get; set; } = null!;
-    }
-}
--- asrc_aps.net/ViewModels/LoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Applied_Sciences_Research_Center_Website.ViewModels
-{
-    public class LoginViewModel
-    {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-    }
-}
