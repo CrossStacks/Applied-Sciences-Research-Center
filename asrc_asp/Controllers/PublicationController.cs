23bf01447b3851838e8c6503ecdc33c0a9c6365d
Muzzammil Imran <m.muzzammil.imran@gmail.com>
2025-03-16 00:52:47 +0500

    Backend: Publication Query update

--- asrc_asp/Controllers/PublicationController.cs
+++ asrc_asp/Controllers/PublicationController.cs
@@ -19,7 +19,7 @@ namespace asrc_asp.Controllers
         public async Task<IActionResult> GetPublications()
         {
             using IDbConnection db = new MySqlConnection(_connectionString);
-            var data = await db.QueryAsync("SELECT j.journal_id, j.path, js.setting_value AS description, s.submission_id, s.date_submitted, s.status, sf.submission_file_id, sf.file_id FROM journals j LEFT JOIN journal_settings js ON j.journal_id = js.journal_id AND js.setting_name = 'description' LEFT JOIN submissions s ON j.journal_id = s.context_id LEFT JOIN submission_files sf ON s.submission_id = sf.submission_id WHERE j.enabled = 1; ");
+            var data = await db.QueryAsync("SELECT j.journal_id, j.path, js_description.setting_value AS description, js_contactName.setting_value AS contact_name, js_contactEmail.setting_value AS contact_email, \r\n       s.submission_id, s.date_submitted, s.status, \r\n       sf.submission_file_id, sf.file_id, \r\n       f.path AS file_path\r\nFROM journals j\r\nLEFT JOIN journal_settings js_description ON j.journal_id = js_description.journal_id AND js_description.setting_name = 'description'\r\nLEFT JOIN journal_settings js_contactName ON j.journal_id = js_contactName.journal_id AND js_contactName.setting_name = 'contactName'\r\nLEFT JOIN journal_settings js_contactEmail ON j.journal_id = js_contactEmail.journal_id AND js_contactEmail.setting_name = 'contactEmail'\r\nLEFT JOIN submissions s ON j.journal_id = s.context_id\r\nLEFT JOIN submission_files sf ON s.submission_id = sf.submission_id\r\nLEFT JOIN files f ON sf.file_id = f.file_id  \r\nWHERE j.enabled = 1;");
             return Ok(data);
         }
     }
