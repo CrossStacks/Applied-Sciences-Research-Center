c3109a9b03090b61e0ca32acffa6026d2155c2ed
Muzzammil Imran <m.muzzammil.imran@gmail.com>
2025-03-15 22:44:41 +0500

    ASP.NET project setuped

---
+++ asrc_asp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asrc_asp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet("isWorking")]
+        public ActionResult isWorking()
+        {
+            return Ok();
+        }
+    }
+}
